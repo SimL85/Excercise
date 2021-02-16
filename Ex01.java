5ab273d7ba6be007e0f4bfecdca168db47064241
Simone L <lentini.simone@gmail.com>
2021-02-16 15:23:37 +0100

    test

---
+++ Ex01.java
@@ -0,0 +1,20 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i+1);
+		}
+		int counter = 0;
+		while (counter < 10) {
+			System.out.println(counter+1);
+			counter++;
+		}
+		int j = 0;
+		do {
+			System.out.println(j+1);
+			j++;
+		} while (j<10);
+	}
+
+}
